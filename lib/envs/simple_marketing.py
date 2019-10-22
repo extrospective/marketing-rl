aa6bb0d8d4d1efc3f7181d807914c04e938115bd
extrospective <markandersen@alum.mit.edu>
2019-10-22 13:33:18 -0400

    Added two state marketing environment

--- lib/envs/simple_marketing.py
+++ lib/envs/simple_marketing.py
@@ -111,6 +111,143 @@ class SimpleMarketingEnv(Environment):
             print('Not rendering non-rgb_array')
 
 
+            
+class TwoValueMarketingEnv(Environment):
+
+    def __init__(self, customer_class, cost_marketing_action=1, p_initial_high=0.3, 
+                 p_low_to_high=0.5, p_high_to_low=0.1, max_steps=5000):
+        super(TwoValueMarketingEnv, self).__init__()
+
+        # define state and action space
+        self.cust = customer_class
+        self.cost_marketing_action = cost_marketing_action
+        self.p_initial_high = p_initial_high
+        self.p_low_to_high = p_low_to_high
+        self.p_high_to_low = p_high_to_low
+        
+        #
+        # Normally one would ask the agent to contain the "guess" about the customer
+        # In this experiment we are asking what would happen if the environment presented
+        # a more complex state: high and low value customers as it made a guess based on
+        # the reward pattern
+        #
+        # p_initial_high = probability that an episode is initialized in high state
+        # p_low_to_high = probability that a purchaser in a low state will be deemed high value state
+        # p_high_to_low = probability that a non-purchaser in a high value state will be deemed low value state
+        
+        #
+        # number of states: There are two sets of states avaiable: 
+        # 
+        # HighValueStates and LowValueStates
+        # 
+        # these are based on an estimate from the environment about what state we are in
+        #
+        # HighValueState = state where customer is believed to be a high value customer
+        # LowValueState = state where customer is believe to be a low value customer  
+        self.shape = (2, self.cust.get_max_states())
+        self.S = np.zeros(self.shape, dtype=np.bool)
+        #
+        # still two actions: market or do not market at time T
+        self.action_space = ActionSpace(range(2))
+
+        print('In this two value marketing environment, actions 0=no marketing action, 1=marketing action')
+        print('Marketing actions will only work in a range of possible steps.')
+        print('Additionally there are high value and low value states provided by the environment')
+        self.max_steps = max_steps
+        self.elig_min, self.elig_max = self.cust.get_eligibility_window()
+        self.reset()
+        
+    def _convert_state(self, state):
+        converted = np.unravel_index(state, self.shape)
+        return np.asarray(list(converted), dtype=np.float32)
+
+    def step(self, action):
+        s_prev = self.s
+        purchase = 0
+        #print('taking step')
+        self.nstep += 1
+        if self.nstep > self.max_steps:
+            print('Aborting due to max steps %d reached' % self.max_steps)
+            raise('too many steps')
+            
+        # moves to next state.  If the customer suddenly died that happens here
+        #
+        #  One flaw is we do not see the cost of marketing to the dead customer I think
+        #
+        self.s = self.cust.get_next_state(s_prev)
+        reward = 0
+        if action >= 1:
+            # Marketing action
+            #print('marketing action occurred')
+            reward = - self.cost_marketing_action
+            
+            # additional reward may occur; and reset to state 0
+            mkt_reward = self.cust.get_reward(self.s)
+            if mkt_reward > 0:
+                # purchased
+                reward += mkt_reward
+                self.s = 0
+                if self.is_low == 1:
+                    if random.random() < self.p_low_to_high:
+                        print('moved to high')
+                        self.is_low = 0
+            else:
+                # not purchased.  may transition from high to low
+                if self.is_low == 0:
+                    if random.random() < self.p_high_to_low:
+                        print('moved to low')
+                        self.is_low = 1
+
+        # Are we at the end for the customer lifetime?
+        if self.cust.get_death(self.s):
+            is_reset = True
+            self.reset()
+        else:
+            is_reset = False
+        #print('step done')  
+        return (self._convert_state(self.s, self.is_low), reward, is_reset, '')
+ 
+    
+    def reset(self):
+        self.nstep = 0
+        self.s = 0
+        self.is_reset = True
+        
+        # randomize high/low
+        self.is_low = 0 if random.random() < self.p_initial_high else 1
+        return self._convert_state(self.s, self.is_low)
+    
+    
+    def _convert_state(self, state, is_low):
+        return np.asarray((is_low, state), dtype=np.float32)
+    
+
+    def render(self, mode='rgb_array', close=False):
+        if close:
+            return
+
+        if mode == 'rgb_array':
+            # 2D: two high, and width = self.tdeath
+            # higher row should be high value
+            
+            # default 1's: yellow
+            # default 0's: purple
+            maze = np.ones((2, self.shape[1]))
+            #
+            # color area where one can market
+            #
+            maze[:, self.elig_min:self.elig_max] = .8
+            
+            #
+            # color the agent: where are they located
+            maze[(self.is_low, self.s)] = 0
+            #print('rendering array')
+            #print(maze)
+            return np.array(maze, copy=True)
+        else:
+            print('Not rendering non-rgb_array')
+
+            
 
 class SimpleCustomerPurchase():
     
